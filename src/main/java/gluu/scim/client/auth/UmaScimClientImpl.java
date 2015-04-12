f1aaf7a9808583634c8ad14268abe4d26e7e858a
yuriyz <yzabrovarniy@gmail.com>
2015-04-12 23:45:50 +0300

    uma : updated scim client to used latest oxauth client (according to latest uma update dated as 02/2015)

--- src/main/java/gluu/scim/client/auth/UmaScimClientImpl.java
+++ src/main/java/gluu/scim/client/auth/UmaScimClientImpl.java
@@ -7,47 +7,39 @@ import gluu.scim.client.exception.ScimInitializationException;
 import gluu.scim.client.model.ScimBulkOperation;
 import gluu.scim.client.model.ScimGroup;
 import gluu.scim.client.model.ScimPerson;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.jboss.resteasy.client.ClientResponseFailure;
+import org.xdi.oxauth.client.uma.CreateRptService;
+import org.xdi.oxauth.client.uma.RptAuthorizationRequestService;
+import org.xdi.oxauth.client.uma.UmaClientFactory;
+import org.xdi.oxauth.client.uma.wrapper.UmaClient;
+import org.xdi.oxauth.model.uma.*;
+import org.xdi.oxauth.model.uma.wrapper.Token;
+import org.xdi.util.StringHelper;
 
+import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.util.concurrent.locks.ReentrantLock;
 
-import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBException;
-
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethodBase;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.jboss.resteasy.client.ClientResponse;
-import org.jboss.resteasy.client.ClientResponseFailure;
-import org.xdi.oxauth.client.uma.AuthorizationRequestService;
-import org.xdi.oxauth.client.uma.RequesterPermissionTokenService;
-import org.xdi.oxauth.client.uma.UmaClientFactory;
-import org.xdi.oxauth.client.uma.wrapper.UmaClient;
-import org.xdi.oxauth.model.uma.AuthorizationResponse;
-import org.xdi.oxauth.model.uma.MetadataConfiguration;
-import org.xdi.oxauth.model.uma.RequesterPermissionTokenResponse;
-import org.xdi.oxauth.model.uma.ResourceSetPermissionTicket;
-import org.xdi.oxauth.model.uma.RptAuthorizationRequest;
-import org.xdi.oxauth.model.uma.wrapper.Token;
-import org.xdi.util.StringHelper;
-
 /**
  * SCIM UMA client
  * 
- * @author Yuriy Movchan Date: 08/23/2013
+ * @author Yuriy Movchan
+ * @author Yuriy Zabrovarnyy
  */
 public class UmaScimClientImpl extends BaseScimClientImpl {
 
 	private static final long serialVersionUID = 7099883500099353832L;
 
 	// UMA
-	private MetadataConfiguration metadataConfiguration;
+	private UmaConfiguration metadataConfiguration;
 	private Token umaAat;
-	private RequesterPermissionTokenResponse umaRpt;
+	private RPTResponse umaRpt;
 
 	private String umaMetaDataUrl;
 
@@ -72,7 +64,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 
 	@Override
 	protected void addAuthenticationHeader(HttpMethodBase httpMethod) {
-		httpMethod.setRequestHeader("Authorization", "Bearer " + this.umaRpt.getToken());
+		httpMethod.setRequestHeader("Authorization", "Bearer " + this.umaRpt.getRpt());
 		httpMethod.setRequestHeader(AuthMode.BEARER_TOKEN_TYPE_HEADER, "uma");
 	}
 
@@ -100,7 +92,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 		if ((this.umaAat == null) || (this.umaAat.getAccessToken() == null) || (this.umaAatAccessTokenExpiration <= now)) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
@@ -131,12 +123,12 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 			throw new ScimInitializationException("Failed to get UMA AAT token");
 		}
 
-		RequesterPermissionTokenService rptService = UmaClientFactory.instance().createRequesterPermissionTokenService(metadataConfiguration);
+		CreateRptService rptService = UmaClientFactory.instance().createRequesterPermissionTokenService(metadataConfiguration);
 
 		// Get RPT
 		this.umaRpt = null;
 		try {
-			umaRpt = rptService.getRequesterPermissionToken("Bearer " + this.umaAat.getAccessToken(), getHost(metadataConfiguration.getIssuer()));
+			umaRpt = rptService.createRPT("Bearer " + this.umaAat.getAccessToken(), getHost(metadataConfiguration.getIssuer()));
 		} catch (ClientResponseFailure ex) {
 			String errorMessage = (String) ex.getResponse().getEntity(String.class);
 			throw new ScimInitializationException("Failed to get RPT token. Error: " + errorMessage, ex);
@@ -173,18 +165,15 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	private boolean authorizeRpt(String ticket) {
-        // Authorize RPT token to access permission ticket
-        ClientResponse<AuthorizationResponse> clientAuthorizationResponse = null;
         try {
-            RptAuthorizationRequest rptAuthorizationRequest = new RptAuthorizationRequest(umaRpt.getToken(), ticket);
-            AuthorizationRequestService authorizationRequestService = UmaClientFactory.instance().createAuthorizationRequestService(metadataConfiguration); 
+            RptAuthorizationRequest rptAuthorizationRequest = new RptAuthorizationRequest(umaRpt.getRpt(), ticket);
+			RptAuthorizationRequestService authorizationRequestService = UmaClientFactory.instance().createAuthorizationRequestService(metadataConfiguration);
 
-            clientAuthorizationResponse = authorizationRequestService.requestRptPermissionAuthorization(
+			RptAuthorizationResponse rptAuthorizationResponse = authorizationRequestService.requestRptPermissionAuthorization(
                     "Bearer " + umaAat.getAccessToken(),
                     getHost(metadataConfiguration.getIssuer()),
                     rptAuthorizationRequest);
-            AuthorizationResponse authorizationResponse = clientAuthorizationResponse.getEntity(AuthorizationResponse.class);
-            if ((authorizationResponse == null) || (clientAuthorizationResponse.getStatus() != Response.Status.OK.getStatusCode())) {
+            if (rptAuthorizationResponse == null) {
             	throw new ScimInitializationException("UMA ticket authorization response is invalid");
             }
             
@@ -194,11 +183,13 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 			throw new ScimInitializationException("Failed to authorize UMA ticket. Error: " + errorMessage, ex);
         } catch (MalformedURLException ex) {
 			throw new ScimInitializationException("Failed to determine host by URI", ex);
+		} catch (Exception ex) {
+			throw new ScimInitializationException(ex.getMessage(), ex);
 		}
 	}
 
 	@Override
-	public ScimResponse retrievePerson(String uid, String mediaType) throws HttpException, IOException {
+	public ScimResponse retrievePerson(String uid, String mediaType) throws IOException {
 		ScimResponse scimResponse = super.retrievePerson(uid, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.retrievePerson(uid, mediaType);
@@ -208,8 +199,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse createPerson(ScimPerson person, String mediaType) throws JsonGenerationException, JsonMappingException,
-			IOException, JAXBException {
+	public ScimResponse createPerson(ScimPerson person, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.createPerson(person, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.createPerson(person, mediaType);
@@ -219,8 +209,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse updatePerson(ScimPerson person, String uid, String mediaType) throws JsonGenerationException, JsonMappingException,
-			UnsupportedEncodingException, IOException, JAXBException {
+	public ScimResponse updatePerson(ScimPerson person, String uid, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.updatePerson(person, uid, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.updatePerson(person, uid, mediaType);
@@ -230,7 +219,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse deletePerson(String uid) throws HttpException, IOException {
+	public ScimResponse deletePerson(String uid) throws IOException {
 		ScimResponse scimResponse = super.deletePerson(uid);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.deletePerson(uid);
@@ -240,7 +229,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse retrieveGroup(String id, String mediaType) throws HttpException, IOException {
+	public ScimResponse retrieveGroup(String id, String mediaType) throws IOException {
 		ScimResponse scimResponse = super.retrieveGroup(id, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.retrieveGroup(id, mediaType);
@@ -250,8 +239,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse createGroup(ScimGroup group, String mediaType) throws JsonGenerationException, JsonMappingException,
-			UnsupportedEncodingException, IOException, JAXBException {
+	public ScimResponse createGroup(ScimGroup group, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.createGroup(group, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.createGroup(group, mediaType);
@@ -272,7 +260,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse deleteGroup(String id) throws HttpException, IOException {
+	public ScimResponse deleteGroup(String id) throws IOException {
 		ScimResponse scimResponse = super.deleteGroup(id);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.deleteGroup(id);
@@ -282,8 +270,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse createPersonString(String person, String mediaType) throws JsonGenerationException, JsonMappingException,
-			IOException, JAXBException {
+	public ScimResponse createPersonString(String person, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.createPersonString(person, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.createPersonString(person, mediaType);
@@ -304,8 +291,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse createGroupString(String group, String mediaType) throws JsonGenerationException, JsonMappingException,
-			UnsupportedEncodingException, IOException, JAXBException {
+	public ScimResponse createGroupString(String group, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.createGroupString(group, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.createGroupString(group, mediaType);
@@ -315,8 +301,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse updateGroupString(String group, String id, String mediaType) throws JsonGenerationException, JsonMappingException,
-			UnsupportedEncodingException, IOException, JAXBException {
+	public ScimResponse updateGroupString(String group, String id, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.updateGroupString(group, id, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.updateGroupString(group, id, mediaType);
@@ -326,8 +311,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse bulkOperation(ScimBulkOperation operation, String mediaType) throws JsonGenerationException, JsonMappingException,
-			UnsupportedEncodingException, IOException, JAXBException {
+	public ScimResponse bulkOperation(ScimBulkOperation operation, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.bulkOperation(operation, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.bulkOperation(operation, mediaType);
@@ -337,7 +321,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse bulkOperationString(String operation, String mediaType) throws HttpException, IOException {
+	public ScimResponse bulkOperationString(String operation, String mediaType) throws IOException {
 		ScimResponse scimResponse = super.bulkOperationString(operation, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.bulkOperationString(operation, mediaType);
@@ -347,7 +331,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse retrieveAllPersons(String mediaType) throws HttpException, IOException {
+	public ScimResponse retrieveAllPersons(String mediaType) throws IOException {
 		ScimResponse scimResponse = super.retrieveAllPersons(mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.retrieveAllPersons(mediaType);
@@ -357,7 +341,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse retrieveAllGroups(String mediaType) throws HttpException, IOException {
+	public ScimResponse retrieveAllGroups(String mediaType) throws IOException {
 		ScimResponse scimResponse = super.retrieveAllGroups(mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.retrieveAllGroups(mediaType);
@@ -367,8 +351,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 	}
 
 	@Override
-	public ScimResponse personSearch(String attribute, String value, String mediaType) throws JsonGenerationException,
-			JsonMappingException, IOException, JAXBException {
+	public ScimResponse personSearch(String attribute, String value, String mediaType) throws IOException, JAXBException {
 		ScimResponse scimResponse = super.personSearch(attribute, value, mediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.personSearch(attribute, value, mediaType);
@@ -379,7 +362,7 @@ public class UmaScimClientImpl extends BaseScimClientImpl {
 
 	@Override
 	public ScimResponse personSearchByObject(String attribute, Object value, String valueMediaType, String outPutMediaType)
-			throws JsonGenerationException, JsonMappingException, IOException, JAXBException {
+			throws IOException, JAXBException {
 		ScimResponse scimResponse = super.personSearchByObject(attribute, value, valueMediaType, outPutMediaType);
 		if (autorizeRpt(scimResponse)) {
             scimResponse = super.personSearchByObject(attribute, value, valueMediaType, outPutMediaType);
