63ee356265ae19815fbdaa79be8aa212fbce67eb
shekhar16 <shekhar@gluu.org>
2016-02-07 21:39:45 +0530

    issues-10 moved the configurations to properties file and created profile in pom.xml.

---
+++ src/test/java/gluu/BaseScimTest.java
@@ -0,0 +1,60 @@
+/*
+ * oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2014, Gluu
+ */
+
+package gluu;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.testng.ITestContext;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeSuite;
+import org.xdi.oxauth.model.util.SecurityProviderUtility;
+import org.xdi.util.StringHelper;
+
+/**
+ * @author Shekhar L
+ * @version February 7, 2016
+ */
+public abstract class BaseScimTest {
+	
+    @BeforeSuite
+    public void initTestSuite(ITestContext context) throws FileNotFoundException, IOException {
+        SecurityProviderUtility.installBCProvider();
+
+        Reporter.log("Invoked init test suite method \n", true);
+
+        String propertiesFile = context.getCurrentXmlTest().getParameter("propertiesFile");
+        if (StringHelper.isEmpty(propertiesFile)) {
+            propertiesFile = "target/test-classes/testng.properties";
+        }
+
+        // Load test paramters
+        FileInputStream conf = new FileInputStream(propertiesFile);
+        Properties prop = new Properties();
+        prop.load(conf);
+
+        Map<String, String> parameters = new HashMap<String, String>();
+        for (Entry<Object, Object> entry : prop.entrySet()) {
+            Object key = entry.getKey();
+            Object value = entry.getValue();
+
+            if (StringHelper.isEmptyString(key) || StringHelper.isEmptyString(value)) {
+                continue;
+            }
+            parameters.put(key.toString(), value.toString());
+        }
+
+        // Overrided test paramters
+        context.getSuite().getXmlSuite().setParameters(parameters);
+    }
+
+  }
