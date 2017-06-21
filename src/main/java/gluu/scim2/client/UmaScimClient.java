bb2c338e3ebd7d0c401a2ffe37a7f6943b761763
Yuriy Zabrovarnyy <yzabrovarniy@gmail.com>
2017-06-21 15:22:31 +0300

    SCIM : adapted scim code to UMA 2 https://github.com/GluuFederation/oxAuth/issues/300

--- src/main/java/gluu/scim2/client/UmaScimClient.java
+++ src/main/java/gluu/scim2/client/UmaScimClient.java
@@ -6,18 +6,20 @@
 package gluu.scim2.client;
 
 import gluu.scim.client.exception.ScimInitializationException;
+import org.apache.commons.lang.StringUtils;
 import org.jboss.resteasy.client.core.BaseClientResponse;
-import org.xdi.oxauth.client.uma.CreateRptService;
-import org.xdi.oxauth.client.uma.RptAuthorizationRequestService;
+import org.xdi.oxauth.client.TokenRequest;
 import org.xdi.oxauth.client.uma.UmaClientFactory;
-import org.xdi.oxauth.client.uma.wrapper.UmaClient;
-import org.xdi.oxauth.model.uma.*;
+import org.xdi.oxauth.client.uma.UmaTokenService;
+import org.xdi.oxauth.model.common.AuthenticationMethod;
+import org.xdi.oxauth.model.common.GrantType;
+import org.xdi.oxauth.model.uma.PermissionTicket;
+import org.xdi.oxauth.model.uma.UmaMetadata;
+import org.xdi.oxauth.model.uma.UmaTokenResponse;
 import org.xdi.oxauth.model.uma.wrapper.Token;
 import org.xdi.util.StringHelper;
 
 import javax.ws.rs.core.Response;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Calendar;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -34,9 +36,9 @@ public class UmaScimClient extends AbstractScimClient {
     private static final long serialVersionUID = 7099883500099353832L;
 
     // UMA
-    private UmaConfiguration metadataConfiguration;
+    private UmaMetadata umaMetadata;
     private Token umaAat;
-    private RPTResponse umaRpt;
+    private String rpt;
 
     private String umaMetaDataUrl;
 
@@ -65,7 +67,7 @@ public class UmaScimClient extends AbstractScimClient {
 
     @Override
     protected String getAuthenticationHeader() {
-        return "Bearer " + this.umaRpt.getRpt();
+        return "Bearer " + rpt;
     }
 
     @Override
@@ -81,7 +83,7 @@ public class UmaScimClient extends AbstractScimClient {
             throw new ScimInitializationException("UMA ticket is invalid", ex);
         }
 
-        return authorizeRpt(permissionTicket.getTicket());
+        return obtainAuthorizedRpt(permissionTicket.getTicket());
     }
 
     private void initUmaAuthentication() {
@@ -110,23 +112,18 @@ public class UmaScimClient extends AbstractScimClient {
     }
 
     private boolean isValidToken(final long now) {
-        if ((this.umaAat == null) || (this.umaAat.getAccessToken() == null) || (this.umaAatAccessTokenExpiration <= now)) {
-            return false;
-        }
+        return this.umaAat != null && this.umaAat.getAccessToken() != null && !(this.umaAatAccessTokenExpiration <= now);
 
-        return true;
     }
 
     private void initUmaRpt() {
         // Remove old tokens
         this.umaAat = null;
-        this.umaRpt = null;
 
         // Get metadata configuration
-        this.metadataConfiguration = UmaClientFactory.instance().createMetaDataConfigurationService(umaMetaDataUrl)
-                .getMetadataConfiguration();
+        this.umaMetadata = UmaClientFactory.instance().createMetadataService(umaMetaDataUrl).getMetadata();
 
-        if ((metadataConfiguration == null) || !StringHelper.equals(metadataConfiguration.getVersion(), "1.0")) {
+        if ((umaMetadata == null)) {
             throw new ScimInitializationException("Failed to load valid UMA metadata configuration");
         }
 
@@ -135,7 +132,19 @@ public class UmaScimClient extends AbstractScimClient {
             if (StringHelper.isEmpty(umaAatClientJksPath) || StringHelper.isEmpty(umaAatClientJksPassword)) {
                 throw new ScimInitializationException("UMA JKS keystore path or password is empty");
             }
-            this.umaAat = UmaClient.requestAat(metadataConfiguration.getTokenEndpoint(), umaAatClientJksPath, umaAatClientJksPassword, umaAatClientId, umaAatClientKeyId);
+
+            TokenRequest tokenRequest = new TokenRequest(GrantType.CLIENT_CREDENTIALS);
+            tokenRequest.setAuthenticationMethod(AuthenticationMethod.PRIVATE_KEY_JWT);
+            tokenRequest.setAuthUsername(umaAatClientId);
+
+            // todo UMA 2 : obtain token correctly
+//            tokenRequest.setCryptoProvider(cryptoProvider);
+//            tokenRequest.setAlgorithm(cryptoProvider.getSignatureAlgorithm(keyId));
+//            tokenRequest.setKeyId(keyId);
+//            tokenRequest.setAudience(umaMetadata.getTokenEndpoint());
+//
+//            this.umaAat = UmaClient.request(umaMetadata.getTokenEndpoint(), tokenRequest);
+            this.umaAat = null;
         } catch (Exception ex) {
             throw new ScimInitializationException("Failed to get AAT token", ex);
         }
@@ -143,40 +152,39 @@ public class UmaScimClient extends AbstractScimClient {
         if (this.umaAat == null) {
             throw new ScimInitializationException("Failed to get UMA AAT token");
         }
+    }
 
-        CreateRptService rptService = null;
-        rptService = UmaClientFactory.instance().createRequesterPermissionTokenService(metadataConfiguration);
-        // Get RPT
-        this.umaRpt = null;
+    private String getAuthorizedRpt(String ticket) {
         try {
-            umaRpt = rptService.createRPT("Bearer " + this.umaAat.getAccessToken(), new URL(metadataConfiguration.getIssuer()).getHost());
-        } catch (MalformedURLException ex) {
-            throw new ScimInitializationException("Failed to determine host by URI", ex);
-        }
 
-        if (this.umaRpt == null) {
-            throw new ScimInitializationException("Failed to get UMA RPT token");
+            // oxauth 3.1.0 supports only UMA 2 (no UMA 1.0.1 anymore),
+            // Spec: https://docs.kantarainitiative.org/uma/ed/oauth-uma-grant-2.0-04.html#rfc.section.3.3.1
+            // Since it is back-channel call (no user interaction) claimToken must contain all cliams that are used in RPT Authorization Policy Script
+            // in our case cliamsToken is idToken. Please obtain id_token with all claims that are required by RPT script.
+            String idToken = null; // todo id token with all claims that are used by RPT Authorization Policy script.
+            String claimTokenFormat = "http://openid.net/specs/openid-connect-core-1_0.html#IDToken";
+
+            UmaTokenService tokenService = UmaClientFactory.instance().createTokenService(umaMetaDataUrl);
+            UmaTokenResponse rptResponse = tokenService.requestRpt("Bearer " + umaAat.getAccessToken(), GrantType.OXAUTH_UMA_TICKET.getValue(),
+                    ticket, idToken, claimTokenFormat, null, null, null);
+
+            if (rptResponse == null) {
+                throw new ScimInitializationException("UMA RPT token response is invalid");
+            }
+            if (StringUtils.isBlank(rptResponse.getAccessToken())) {
+                throw new ScimInitializationException("UMA RPT is invalid");
+            }
+
+            rpt = rptResponse.getAccessToken();
+            return rpt;
+        } catch (Exception ex) {
+            throw new ScimInitializationException(ex.getMessage(), ex);
         }
     }
 
-    private boolean authorizeRpt(String ticket) {
+    private boolean obtainAuthorizedRpt(String ticket) {
         try {
-            RptAuthorizationRequestService authorizationRequestService = null;
-            RptAuthorizationRequest rptAuthorizationRequest = new RptAuthorizationRequest(umaRpt.getRpt(), ticket);
-
-            authorizationRequestService = UmaClientFactory.instance().createAuthorizationRequestService(metadataConfiguration);
-
-            RptAuthorizationResponse rptAuthorizationResponse = authorizationRequestService.requestRptPermissionAuthorization(
-                    "Bearer " + umaAat.getAccessToken(),
-                    new URL(metadataConfiguration.getIssuer()).getHost(),
-                    rptAuthorizationRequest);
-            if (rptAuthorizationResponse == null) {
-                throw new ScimInitializationException("UMA ticket authorization response is invalid");
-            }
-
-            return true;
-        } catch (MalformedURLException ex) {
-            throw new ScimInitializationException("Failed to determine host by URI", ex);
+            return StringUtils.isNotBlank(getAuthorizedRpt(ticket));
         } catch (Exception ex) {
             throw new ScimInitializationException(ex.getMessage(), ex);
         }
