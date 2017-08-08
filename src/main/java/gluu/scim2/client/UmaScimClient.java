99ec8a6fff82d01030fbe6139c3f4b88c139ea74
yurem <Yuriy.Movchan@gmail.com>
2017-08-08 22:05:29 +0300

    Update SCIM client to conform UMA 2.0

--- src/main/java/gluu/scim2/client/UmaScimClient.java
+++ src/main/java/gluu/scim2/client/UmaScimClient.java
@@ -22,6 +22,8 @@ import org.xdi.oxauth.client.uma.wrapper.UmaClient;
 import org.xdi.oxauth.model.common.AuthenticationMethod;
 import org.xdi.oxauth.model.common.GrantType;
 import org.xdi.oxauth.model.crypto.OxAuthCryptoProvider;
+import org.xdi.oxauth.model.token.ClientAssertionType;
+import org.xdi.oxauth.model.uma.ClaimTokenFormatType;
 import org.xdi.oxauth.model.uma.UmaMetadata;
 import org.xdi.oxauth.model.uma.UmaTokenResponse;
 import org.xdi.oxauth.model.uma.wrapper.Token;
@@ -42,11 +44,7 @@ public class UmaScimClient extends AbstractScimClient {
     private static final long serialVersionUID = 7099883500099353832L;
 
     // UMA
-    private UmaMetadata umaMetadata;
-    private Token umaAat;
     private String rpt;
-
-    private String umaMetaDataUrl;
 
     private String umaAatClientId;
     private String umaAatClientKeyId;
@@ -57,9 +55,8 @@ public class UmaScimClient extends AbstractScimClient {
 
     private final ReentrantLock lock = new ReentrantLock();
 
-    public UmaScimClient(String domain, String umaMetaDataUrl, String umaAatClientId, String umaAatClientJksPath, String umaAatClientJksPassword, String umaAatClientKeyId) {
+    public UmaScimClient(String domain, String umaAatClientId, String umaAatClientJksPath, String umaAatClientJksPassword, String umaAatClientKeyId) {
         super(domain);
-        this.umaMetaDataUrl = umaMetaDataUrl;
         this.umaAatClientId = umaAatClientId;
         this.umaAatClientJksPath = umaAatClientJksPath;
         this.umaAatClientJksPassword = umaAatClientJksPassword;
@@ -68,7 +65,6 @@ public class UmaScimClient extends AbstractScimClient {
 
     @Override
     protected void prepareRequest() {
-        initUmaAuthentication();
     }
 
     @Override
@@ -95,63 +91,25 @@ public class UmaScimClient extends AbstractScimClient {
         }
         
         String permissionTicket = null;
+        String asUri = null;
         String[] headerKeyValues = StringHelper.split(permissionTicketResponse.toString(), ",");
         for (int i = 0; i < headerKeyValues.length; i++) {
         	if (headerKeyValues[i].startsWith("ticket=")) {
         		permissionTicket = headerKeyValues[i].substring(7);
         	}
+        	if (headerKeyValues[i].startsWith("as_uri=")) {
+        		asUri = headerKeyValues[i].substring(7);
+        	}
 		}
 		
-		if (StringHelper.isEmpty(permissionTicket)) {
+		if (StringHelper.isEmpty(asUri) || StringHelper.isEmpty(permissionTicket)) {
 			return false;
 		}
 
-        return obtainAuthorizedRpt(permissionTicket);
+        return obtainAuthorizedRpt(asUri, permissionTicket);
     }
 
-    private void initUmaAuthentication() {
-        long now = System.currentTimeMillis();
-
-        // Get new access token only if is the previous one is missing or expired
-        if (!isValidToken(now)) {
-            lock.lock();
-            try {
-                now = System.currentTimeMillis();
-                if (!isValidToken(now)) {
-                    initUmaAat();
-                    Calendar calendar = Calendar.getInstance();
-                    if (this.umaAat.getExpiresIn() != null) {
-                        calendar.add(Calendar.SECOND, this.umaAat.getExpiresIn());
-                        calendar.add(Calendar.SECOND, -10); // Subtract 10 seconds to avoid
-                    }
-                    this.umaAatAccessTokenExpiration = calendar.getTimeInMillis();
-                }
-            } catch (Exception ex) {
-                throw new ScimInitializationException("Could not get accessToken", ex);
-            } finally {
-                lock.unlock();
-            }
-        }
-    }
-
-    private boolean isValidToken(final long now) {
-        return this.umaAat != null && this.umaAat.getAccessToken() != null && !(this.umaAatAccessTokenExpiration <= now);
-
-    }
-
-    private void initUmaAat() {
-        //the same as gluu.scim.client.auth.UmaScimClientImpl.initAccessToken() !?
-        // Remove old tokens
-        this.umaAat = null;
-
-        // Get metadata configuration
-        this.umaMetadata = UmaClientFactory.instance().createMetadataService(umaMetaDataUrl).getMetadata();
-
-        if (umaMetadata == null) {
-            throw new ScimInitializationException("Failed to load valid UMA metadata configuration");
-        }
-
-        // Get AAT
+    private TokenRequest getAuthorizationTokerRequest(UmaMetadata umaMetadata) {
         try {
             if (StringHelper.isEmpty(umaAatClientJksPath) || StringHelper.isEmpty(umaAatClientJksPassword)) {
                 throw new ScimInitializationException("UMA JKS keystore path or password is empty");
@@ -185,25 +143,25 @@ public class UmaScimClient extends AbstractScimClient {
             tokenRequest.setKeyId(keyId);
             tokenRequest.setAudience(umaMetadata.getTokenEndpoint());
 
-            this.umaAat = UmaClient.request(umaMetadata.getTokenEndpoint(), tokenRequest);
-
+            return tokenRequest;
         } catch (Exception ex) {
-            throw new ScimInitializationException("Failed to get AAT token", ex);
-        }
-
-        if (this.umaAat == null) {
-            throw new ScimInitializationException("Failed to get UMA AAT token");
+            throw new ScimInitializationException("Failed to get client token", ex);
         }
     }
 
-    private String getAuthorizedRpt(String ticket) {
+    private String getAuthorizedRpt(String asUri, String ticket) {
         try {
+        	// Get metadata configuration
+        	UmaMetadata umaMetadata = UmaClientFactory.instance().createMetadataService(asUri).getMetadata();
+            if (umaMetadata == null) {
+                throw new ScimInitializationException(String.format("Failed to load valid UMA metadata configuration from: %s", asUri));
+            }
+
+        	TokenRequest tokenRequest = getAuthorizationTokerRequest(umaMetadata);
             //No need for claims token. See comments on issue https://github.com/GluuFederation/SCIM-Client/issues/22
-            String claimTokenFormat = "http://openid.net/specs/openid-connect-core-1_0.html#IDToken";
-            String authzHeader = "Bearer " + umaAat.getAccessToken();
 
             UmaTokenService tokenService = UmaClientFactory.instance().createTokenService(umaMetadata);
-            UmaTokenResponse rptResponse = tokenService.requestRpt(authzHeader, GrantType.OXAUTH_UMA_TICKET.getValue(), ticket, null, claimTokenFormat, null, null, null);
+            UmaTokenResponse rptResponse = tokenService.requestJwtAuthorizationRpt(ClientAssertionType.JWT_BEARER.toString(), tokenRequest.getClientAssertion(), GrantType.OXAUTH_UMA_TICKET.getValue(), ticket, null, null/*ClaimTokenFormatType.ID_TOKEN.getValue()*/, null, null, null);
 
             if (rptResponse == null) {
                 throw new ScimInitializationException("UMA RPT token response is invalid");
@@ -220,9 +178,9 @@ public class UmaScimClient extends AbstractScimClient {
         }
     }
 
-    private boolean obtainAuthorizedRpt(String ticket) {
+    private boolean obtainAuthorizedRpt(String asUri, String ticket) {
         try {
-            return StringUtils.isNotBlank(getAuthorizedRpt(ticket));
+            return StringUtils.isNotBlank(getAuthorizedRpt(asUri, ticket));
         } catch (Exception ex) {
             throw new ScimInitializationException(ex.getMessage(), ex);
         }
