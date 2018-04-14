1a278b363f474c372b9e6ae249fc074dd4547c63
Jose G <bonustrack310@gmail.com>
2018-04-14 08:01:04 -0500

    Polish simple sample test

--- scim-client2/src/test/java/gluu/scim2/client/SampleTest.java
+++ scim-client2/src/test/java/gluu/scim2/client/SampleTest.java
@@ -8,20 +8,16 @@ package gluu.scim2.client;
 import gluu.scim2.client.factory.ScimClientFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.gluu.oxtrust.model.scim2.Constants;
-import org.gluu.oxtrust.model.scim2.CustomAttributes;
-import org.gluu.oxtrust.model.scim2.ErrorResponse;
-import org.gluu.oxtrust.model.scim2.SearchRequest;
-import org.gluu.oxtrust.model.scim2.user.Email;
+import org.gluu.oxtrust.model.scim2.*;
 import org.gluu.oxtrust.model.scim2.user.UserResource;
 import org.gluu.oxtrust.ws.rs.scim2.IUserWebService;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.Response;
-import java.util.Collections;
 
-import javax.ws.rs.core.Response.Status;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.testng.Assert.*;
 
 /**
  * Created by jgomer on 2017-09-14.
@@ -30,27 +26,41 @@ public class SampleTest extends BaseTest {
 
     private Logger logger = LogManager.getLogger(getClass());
 
-    @Parameters({"domainURL", "OIDCMetadataUrl"})
     @Test
-    public void smallerClient(String domain, String url) throws Exception{
-        IUserWebService service =ScimClientFactory.getTestClient(IUserWebService.class, domain, url);
+    @Parameters ({"domainURL", "umaAatClientId", "umaAatClientJksPath", "umaAatClientJksPassword", "umaAatClientKeyId"})
+    public void smallerClient(String domainURL, String umaAatClientId, String umaAatClientJksPath, String umaAatClientJksPassword,
+                              String umaAatClientKeyId) throws Exception {
+
+        IUserWebService myclient = ScimClientFactory.getClient(IUserWebService.class, domainURL, umaAatClientId,
+                umaAatClientJksPath, umaAatClientJksPassword, umaAatClientKeyId);
 
         SearchRequest sr=new SearchRequest();
         sr.setFilter("userName eq \"admin\"");
-        Response r= service.searchUsersPost(sr);
 
-        UserResource u=r.readEntity(UserResource.class);
-        logger.debug("Admin AKA {}", u.getDisplayName());
+        Response response = myclient.searchUsersPost(sr);
+        assertEquals(response.getStatus(), OK.getStatusCode());
+
+        UserResource u = (UserResource) response.readEntity(ListResponse.class).getResources().get(0);
+        logger.debug("Hello {}!", u.getDisplayName());
 
     }
 
-    @Test
-    public void mytest() throws Exception{
+    //@Test
+    @Parameters({"domainURL", "OIDCMetadataUrl"})
+    //This test showcases test mode usage (not typical UMA protection mode). Run only under such condition
+    public void testModeTest(String domain, String url) throws Exception{
 
-        Response response=client.getUserById("...", null, null);
-        logger.debug(response.readEntity(String.class));
+        IUserWebService myclient = ScimClientFactory.getTestClient(IUserWebService.class, domain, url);
+
+        SearchRequest sr=new SearchRequest();
+        sr.setFilter("pairwiseIdentitifers pr");
+        sr.setSortBy("meta.lastModified");
+
+        Response response = myclient.searchUsersPost(sr);
+        assertEquals(response.getStatus(), OK.getStatusCode());
+
+        logger.debug("There are {} users with PPIDs associated", response.readEntity(ListResponse.class).getResources().size());
 
     }
 
 }
-
