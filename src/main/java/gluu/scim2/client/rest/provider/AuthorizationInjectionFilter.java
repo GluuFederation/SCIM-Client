90150e3f7d85523f3d1f38998763af4de42dbdef
Jose G <bonustrack310@gmail.com>
2018-11-13 16:06:31 -0500

    Inject custom headers from java variables

--- src/main/java/gluu/scim2/client/rest/provider/AuthorizationInjectionFilter.java
+++ src/main/java/gluu/scim2/client/rest/provider/AuthorizationInjectionFilter.java
@@ -12,10 +12,14 @@ import org.apache.logging.log4j.Logger;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 
 /**
- * A Client-side filter employed to "inject" the Authorization header to the outgoing request. This filter applies only for
+ * A Client-side filter employed to "inject" headers to the outgoing request. This filter applies only for
  * requests issued by concrete instances of {@link gluu.scim2.client.AbstractScimClient AbstractScimClient} class.
  * <p>Developers do not need to manipulate this class for their SCIM applications.</p>
  */
@@ -27,11 +31,22 @@ public class AuthorizationInjectionFilter implements ClientRequestFilter {
 
     private Logger logger = LogManager.getLogger(getClass());
 
-    public void filter(ClientRequestContext context){
+    public void filter(ClientRequestContext context) {
 
-        String authzHeader=ClientMap.getValue(context.getClient());
-        if (StringUtils.isNotEmpty(authzHeader))    //resteasy client is tied to an authz header
-            context.getHeaders().putSingle("Authorization", authzHeader);
+        MultivaluedMap<String, Object> headers = context.getHeaders();
+        String authzHeader = ClientMap.getValue(context.getClient());
+
+        if (StringUtils.isNotEmpty(authzHeader)) {   //resteasy client is tied to an authz header
+            headers.putSingle("Authorization", authzHeader);
+        }
+        //Inject custom headers
+        Optional.ofNullable(System.getProperty("scim.extraHeaders"))
+                .map(str -> Arrays.asList(str.split(",\\s*"))).orElse(Collections.emptyList())
+                .forEach(prop ->
+                        Optional.ofNullable(System.getProperty("scim.header." + prop))
+                                .ifPresent(value -> headers.putSingle(prop,  value))
+                );
+
     }
 
 }
